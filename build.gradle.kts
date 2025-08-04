ed40ea287148e5660f6db7f707eed13cdf1ba613
kkgowtham-engg-sdk <120382101+kkgowtham-engg-sdk@users.noreply.github.com>
2025-08-04 15:30:41 +0530

    REL-2201: Update versions and changelog (#26)

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,11 +21,11 @@ plugins {
 
 catalog {
     versionCatalog {
-        library("core", "com.moengage", "moe-android-sdk").version("14.03.00")
-        library("cardsCore", "com.moengage", "cards-core").version("3.1.0")
-        library("cardsUi", "com.moengage", "cards-ui").version("3.1.0")
+        library("core", "com.moengage", "moe-android-sdk").version("14.03.01")
+        library("cardsCore", "com.moengage", "cards-core").version("3.1.1")
+        library("cardsUi", "com.moengage", "cards-ui").version("3.1.1")
         library("geofence", "com.moengage", "geofence").version("5.0.1")
-        library("inapp", "com.moengage", "inapp").version("9.2.0")
+        library("inapp", "com.moengage", "inapp").version("9.2.1")
         library("inboxCore", "com.moengage", "inbox-core").version("4.1.0")
         library("inboxUi", "com.moengage", "inbox-ui").version("4.1.0")
         library("pushKit", "com.moengage", "hms-pushkit").version("6.1.0")
@@ -37,7 +37,7 @@ catalog {
             "moengageSegmentKotlinDestination",
             "com.moengage",
             "moengage-segment-kotlin-destination"
-        ).version("3.2.0")
+        ).version("3.2.1")
         library("encryptedStorage", "com.moengage", "encrypted-storage").version("3.0.0")
         library("moengageMparticleKit", "com.moengage", "mparticle-android-integration-moengage").version("1.1.0")
         bundle(
